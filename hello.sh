df75c79986fa1805ed25133e753eae09a303c5b7
Md. Tanvir Alam Anik <221902283@student.green.edu.bd>
2024-04-02 13:43:10 +0600

    added

---
+++ hello.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+#this is a comment and it will not be executed.
+# echo "Hello world"
+
+#system variable and user variable
+
+# echo our shell name is $BASH
+
+# echo our shell version name is $BASH_VERSION
+
+# echo Our home directory is $HOME
+
+# echo Current working directory $PWD
+
+
+
+
+#name=Tanvir
+
+# id=221902283
+
+# echo My name is $name and my id is $id
+
+# echo What is your name?
+
+# read name1 name2 name3 name4
+
+# echo My name is $name1 $name3
+
+
+read -p 'username : ' username
+
+echo "username" : $username
+
+
+read -sp 'password : ' pass
+
+echo 'password : ' $pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
